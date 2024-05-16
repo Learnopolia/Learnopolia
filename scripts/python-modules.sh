3cfa1724d58cce9b92699571fa27e50fd7ed26ba
dohoudaniel <dohoudanielfavour@gmail.com>
2024-05-16 19:33:18 +0100

    Add Bash scripts to install Python packages

---
+++ scripts/python-modules.sh
@@ -0,0 +1,10 @@
+# !/usr/bin/env bash
+# This script installs the Python modules needed
+# to run Learnopolia on a web server
+
+# Install the Python modules
+pip install flask
+pip install flask_mysqldb
+pip install flask_sqlalchemy
+pip install sqlalchemy
+pip install mysqlclient
