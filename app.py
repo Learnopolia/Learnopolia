b38d3370f6cea26f2bfafb1478a320523af5479f
dohoudaniel <dohoudanielfavour@gmail.com>
2024-05-17 12:10:26 +0100

    Fix image linking, and turned on debug mode.

--- app.py
+++ app.py
@@ -134,4 +134,4 @@ def submit_form():
 if __name__ == '__main__':
     app.secret_key = 'your_secret_key'
     # app.run(debug=True)
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
