9d7fce5f762c68bc3c7de3dbd07646d83c65bf87
dohoudaniel <dohoudanielfavour@gmail.com>
2024-12-19 03:54:20 +0100

    fix: turn off debug mode

--- app.py
+++ app.py
@@ -103,7 +103,7 @@ def subscribe():
 if __name__ == '__main__':
     app.secret_key = 'your_secret_key'
     # app.run(debug=True)
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000) #, debug=True)
 
 
 ### Comments ###
